f845c84339750469a798946acadea43dd83ef86a
michael-duke <michael.21duke@gmail.com>
2022-10-17 22:08:58 +0300

    Add species column into animals table data structure

--- schema.sql
+++ schema.sql
@@ -1,9 +1,22 @@
 /* Database schema to keep the structure of entire database. */
 
 --Monday
+/*
 CREATE TABLE animals (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name varchar(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
+);
+*/
+
+--Tuesday
+CREATE TABLE animals (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    species varchar(100) NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
