d05ddad834b7987bda4d2fe6c220287bcfe57243
michael-duke <michael.21duke@gmail.com>
2022-10-19 15:52:13 +0300

    Create new tables and many to many relationship
    - Create vets table to hold info about the vet doctors
    - Create a Join table specializations to know which vet specializes on which animal type/specie
    - Create a Join table visits that holds data about animal visits and also the day of visit
    - Implement respective foreign keys for all Join tables (Many-Many relationship)

--- schema.sql
+++ schema.sql
@@ -28,6 +28,7 @@ CREATE TABLE animals (
 */
 
 --Wednesday
+/*
 CREATE TABLE owners (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     full_name varchar(100) NOT NULL,
@@ -48,4 +49,46 @@ CREATE TABLE animals (
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL NOT NULL
+);
+*/
+
+--Thursday
+CREATE TABLE owners (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    full_name varchar(100) NOT NULL,
+    age INT NOT NULL
+);
+
+CREATE TABLE species (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+CREATE TABLE animals (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    species_id INT NULL REFERENCES species(id) ON DELETE CASCADE,
+    owner_id INT NULL REFERENCES owners(id) ON DELETE CASCADE,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
+);
+
+CREATE TABLE vets (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+    species_id INT NULL REFERENCES species(id) ON DELETE CASCADE,
+    vet_id INT NULL REFERENCES vets(id) ON DELETE CASCADE
+);
+
+CREATE TABLE visits (
+    animal_id INT NULL REFERENCES animals(id) ON DELETE CASCADE,
+    vet_id INT NULL REFERENCES vets(id) ON DELETE CASCADE,
+    date_of_visit DATE NOT NULL
 );
